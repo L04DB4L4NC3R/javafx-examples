acc1144cb26361f7b73be4bba0a32b7b03b0dc87
Angad Sharma <30529572+L04DB4L4NC3R@users.noreply.github.com>
2020-10-21 10:57:17 +0000

    Update RegistrationFormApplication.java

--- javafx-registration-form-application/src/RegistrationFormApplication.java
+++ javafx-registration-form-application/src/RegistrationFormApplication.java
@@ -18,13 +18,9 @@ public class RegistrationFormApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Registration Form JavaFX Application");
 
-        // Create the registration form grid pane
         GridPane gridPane = createRegistrationFormPane();
-        // Add UI controls to the registration form grid pane
         addUIControls(gridPane);
-        // Create a scene with registration form grid pane as the root node
         Scene scene = new Scene(gridPane, 800, 500);
-        // Set the scene in primary stage
         primaryStage.setScene(scene);
 
         primaryStage.show();
@@ -32,28 +28,18 @@ public class RegistrationFormApplication extends Application {
 
 
     private GridPane createRegistrationFormPane() {
-        // Instantiate a new Grid Pane
         GridPane gridPane = new GridPane();
 
-        // Position the pane at the center of the screen, both vertically and horizontally
         gridPane.setAlignment(Pos.CENTER);
 
-        // Set a padding of 20px on each side
         gridPane.setPadding(new Insets(40, 40, 40, 40));
 
-        // Set the horizontal gap between columns
         gridPane.setHgap(10);
 
-        // Set the vertical gap between rows
         gridPane.setVgap(10);
 
-        // Add Column Constraints
-
-        // columnOneConstraints will be applied to all the nodes placed in column one.
         ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
         columnOneConstraints.setHalignment(HPos.RIGHT);
-
-        // columnTwoConstraints will be applied to all the nodes placed in column two.
         ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
         columnTwoConstrains.setHgrow(Priority.ALWAYS);
 
@@ -63,42 +49,58 @@ public class RegistrationFormApplication extends Application {
     }
 
     private void addUIControls(GridPane gridPane) {
-        // Add Header
         Label headerLabel = new Label("Registration Form");
         headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         gridPane.add(headerLabel, 0,0,2,1);
         GridPane.setHalignment(headerLabel, HPos.CENTER);
         GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
 
-        // Add Name Label
         Label nameLabel = new Label("Full Name : ");
         gridPane.add(nameLabel, 0,1);
 
-        // Add Name Text Field
         TextField nameField = new TextField();
         nameField.setPrefHeight(40);
         gridPane.add(nameField, 1,1);
 
 
-        // Add Email Label
         Label emailLabel = new Label("Email ID : ");
         gridPane.add(emailLabel, 0, 2);
 
-        // Add Email Text Field
         TextField emailField = new TextField();
         emailField.setPrefHeight(40);
         gridPane.add(emailField, 1, 2);
 
-        // Add Password Label
         Label passwordLabel = new Label("Password : ");
         gridPane.add(passwordLabel, 0, 3);
 
-        // Add Password Field
         PasswordField passwordField = new PasswordField();
         passwordField.setPrefHeight(40);
         gridPane.add(passwordField, 1, 3);
 
-        // Add Submit Button
+
+
+Label regLabel = new Label("Regno : ");
+        gridPane.add(regLabel, 0, 4);
+
+        TextField regField = new TextField();
+        regField.setPrefHeight(40);
+        gridPane.add(passwordField, 1, 4);
+
+Label mobileLabel = new Label("Mobile : ");
+        gridPane.add(mobileLabel, 0, 5);
+
+        TextField mobileField = new TextField();
+        mobileField.setPrefHeight(40);
+        gridPane.add(mobileField, 1, 5);
+
+Label cgpaLabel = new Label("CGPA : ");
+        gridPane.add(Label, 0, 6);
+
+        TextField cgpaField = new TextField();
+        cgpaField.setPrefHeight(40);
+        gridPane.add(cgpaField, 1, 6);
+
+
         Button submitButton = new Button("Submit");
         submitButton.setPrefHeight(40);
         submitButton.setDefaultButton(true);
